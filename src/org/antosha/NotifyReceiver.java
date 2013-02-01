5cc2ad5bc1398d7505bd666712a203451aeecc42
antosha.4e@gmail.com <antosha.4e@gmail.com@ef844d68-0de4-4f86-9644-67b378cead23>
2013-02-01 12:46:25 +0000

    ...

--- src/org/antosha/NotifyReceiver.java
+++ src/org/antosha/NotifyReceiver.java
@@ -4,7 +4,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.PowerManager;
+
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -13,17 +15,19 @@ import java.util.concurrent.TimeUnit;
  * Date: 10.05.12
  */
 public class NotifyReceiver extends BroadcastReceiver {
+    private final ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+
     @Override
     public void onReceive(Context context, Intent intent) {
         PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
 
-        final PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK|PowerManager.ACQUIRE_CAUSES_WAKEUP, "Loooolka");
+        final PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "Loooolka");
         wl.acquire();
 
-        Executors.newSingleThreadScheduledExecutor().schedule(new Runnable() {
+        service.schedule(new Runnable() {
             public void run() {
                 wl.release();
             }
-        }, 10, TimeUnit.SECONDS);
+        }, 5, TimeUnit.SECONDS);
     }
 }
